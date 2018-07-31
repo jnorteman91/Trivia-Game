02e723c6365a0e7c985b1ceb59013ac9acc53755
Jd0h Norteman <jnorteman91@gmail.com>
2018-07-31 18:41:21 -0400

    Trivia-Game

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    $("#image").css("display", "none");
+
+    var time = 15;
+    var rightNumber = 0;
+    var wrongNumber = 0;
+    var ansNumber = 0;
+    var answers = [];
+    var question = 0;
+
+    var trivia = [
+        q1 = {
+            question: "An American skate company founded by George Powell?",
+            correct: "2",
+            multipleChoice: ["Birdhouse", "Powell Peralta","Zero","Creature"], 
+        }
+    ];
+})
