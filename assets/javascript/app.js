1e5c8c838e4655c44ab4928e333036fdb3a29b37
Jd0h Norteman <jnorteman91@gmail.com>
2018-07-31 20:32:48 -0400

    Trivia-Game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,76 @@ $(document).ready(function() {
             question: "An American skate company founded by George Powell?",
             correct: "2",
             multipleChoice: ["Birdhouse", "Powell Peralta","Zero","Creature"], 
+        },
+        q2 = {
+            question: "A Canadian who skated for Flip, Alien Workshop, Birdhouse, and Circa?",
+            correct: "4",
+            multipleChoice: ["Colt Cannon", "Sierra Fellers", "Chris Cole", "Mark Appleyard"], 
+        },
+        q3 = {
+            question: "This skateboard company is known for it footwear, it was started by Four Star Distrubution and Chad Muska.",
+            correct: "4",
+            multipleChoice: ["Osiris", "DC", "Vans", "Circa"], 
+        },
+        q4 = {
+            question: "This skatepark is located in Colorado and is a replica of the Street League competetive course.",
+            correct: "1",
+            multipleChoice: ["Erie Skatepark", "Boulder Skatepark", "Lyons Skatepark", "Denver Skatepark"], 
+        },
+        q5 = {
+            question: "This show is a documentary of where skaters came from and where they are now.",
+            correct: "3",
+            multipleChoice: ["King of the Road", "It's Time", "Epicly Later'd", "411 Video Mag"], 
         }
     ];
+
+    var hide = function(elementId) {
+        $(elementId).css("visibility", "hidden");
+    };
+
+    var show = function(elementId) {
+        $(elementId).css("visibility", "visible");
+    };
+
+    var write = function(elementId, this) {
+        $(elementId).html("<h3>" + this + "</h3>")
+    };
+
+    var writeQuestion = function () {
+        if (question <= 4) {
+            $("#questionDiv").html("<h2>" + trivia[question].question + "</h2>");
+            answers = trivia[question].multipleChoice;
+            show(".answers");
+            for (var i = 0; i < answers.length; i++) {
+                $("#answers + i").html("<h3>" + answers[i] + "</h3>"); 
+            }
+        } else {
+            gameEnd();
+        }
+    };
+
+    var clearAnswer = function () {
+        for (var i = 0; i < 4; i++) {
+            $("#answer" + i).html("");
+        }
+        hide(".answer");
+    };
+
+    var startGame = function () {
+        counter = setInterval(countDown, 1000);
+            $("#startTitle").empty();
+        hide("#start");
+        writeQuestion();
+    };
+
+    var screenClear = function () {
+        $("#startTitle").empty();
+        $("#questionDiv").empty();
+        $("#score").empty();
+        answerClear();
+    }
+
+    
+
+
 })
